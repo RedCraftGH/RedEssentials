918f480378d038d3351da74417866daa2649ef48
RedCraftGH <36905383+RedCraftGH@users.noreply.github.com>
2018-10-01 18:14:28 -0400

    Update RedEssentials.php

--- src/RedCraftPE/RedEssentials.php
+++ src/RedCraftPE/RedEssentials.php
@@ -12,16 +12,16 @@ class RedEssentials extends PluginBase implements Listener {
 
   public function onEnable() : void {
   
-    $this->getServer()->info(TextFormat::RED . "RedEssentials is now enabled on " . $this->getServer()->getName());
+    $this->getLogger()->info(TextFormat::RED . "RedEssentials is now enabled on " . $this->getServer()->getName());
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
   public function onDisable() : void {
   
-  $this->getServer()->info(TextFormat::RED . "RedEssentials is now disabled on " . $this->getServer()->getName());
+  $this->getLogger()->info(TextFormat::RED . "RedEssentials is now disabled on " . $this->getServer()->getName());
   }
   public function onLoad() : void {
   
-  $this->getServer()->info(TextFormat::RED . "RedEssentials is now loaded on " . $this->getServer()->getName());
+  $this->getLogger()->info(TextFormat::RED . "RedEssentials is now loaded on " . $this->getServer()->getName());
   }
   public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
   

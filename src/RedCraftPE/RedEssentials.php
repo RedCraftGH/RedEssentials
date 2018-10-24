556ded3e0f93527d3792c2e33b0d00977a20c600
RedCraftGH <36905383+RedCraftGH@users.noreply.github.com>
2018-10-24 17:33:19 -0400

    Update RedEssentials.php

--- src/RedCraftPE/RedEssentials.php
+++ src/RedCraftPE/RedEssentials.php
@@ -392,7 +392,7 @@ class RedEssentials extends PluginBase implements Listener {
               $this->cfg->save();
             } elseif ($args[0] === "off") {
 
-              if (!(in_array($args[1], $this->cfg->get("Void Worlds"))) {
+              if (!(in_array($args[1], $this->cfg->get("Void Worlds")))) {
 
                 $sender->sendMessage($prefix . TextFormat::WHITE . $args[1] . TextFormat::RED . " does not have Safe Void enabled!");
                 return true;
